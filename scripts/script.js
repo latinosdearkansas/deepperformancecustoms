a804ac61ce5d874d5a7419f088d45cd9905fe2f2
Rogelio Mojica <latinosdearkansas@outlook.com>
2021-06-23 12:10:13 -0500

    improved manu bar

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,10 @@
-let roger = 21;
+let navLinks = document.getElementById("navLinks");
+
+function showMenu() {
+    navLinks.style.left ="0";
+}
+
+function hideMenu() {
+    navLinks.style.left = "-160px";
+}
+
